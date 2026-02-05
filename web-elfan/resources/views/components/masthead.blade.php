8beefe5b11ca7cf8cdc3418f7e50dd2299ac923d
ell <ifadussyahidin@gmail.com>
2026-02-05 10:38:16 +0700

    pecahan

---
+++ web-elfan/resources/views/components/masthead.blade.php
@@ -0,0 +1,15 @@
+
+    <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">Elfan AI Academy – Building Digital Leaders with Adabs</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 mb-5">Kami menggabungkan pendidikan karakter dan teknologi untuk melahirkan inovator, kreator, dan pemimpin digital masa depan melalui pembelajaran vokasi berbasis praktik, sertifikasi kompetensi, dan pengalaman industri nyata.!</p>
+                        <a class="btn btn-primary btn-xl" href="#about">Learn more</a>
+                    </div>
+                </div>
+            </div>
+        </header>
